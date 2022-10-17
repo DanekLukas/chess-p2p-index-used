5994eb1af5791a15e49a5e322dfb9c87f66a034e
Lukáš Daněk <dr.danek@gmail.com>
2022-10-17 11:02:18 +0200

    bugfix

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import WebSocket, { WebSocketServer } from 'ws'
 import fs, { readdirSync } from 'fs'
 import { randomBytes } from 'crypto'
@@ -6,15 +6,20 @@ import { randomBytes } from 'crypto'
 import dotenv from 'dotenv'
 import https from 'https'
 import http from 'http'
+import { createClient } from 'redis';
 
 dotenv.config()
 
 type client = {index: string, ws: WebSocket , name: string, room: string, alive: boolean, id: number, usePing: boolean, used: number}
 
-const app: Express = express()
+const appl = async () => {
+
+await redis.connect()
+
+const clients: Record<string, client> = JSON.parse(await redis.get('clients') || '{}')
+
 const pre = ''
 // let intr:NodeJS.Timer|undefined = undefined
-const clients: Record<string, client> = {}
 const protocol = process.env.PROTOCOL === 'https' ? 'https' : 'http'
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || '8080'
@@ -30,19 +35,6 @@ app.set('port', port)
 app.set('host', host)
 
 const staticFldr = 'web-build'
-
-const getCli = () => {
-  return (Object.entries(clients) as Array<[string, client]>).map((item:[string, client]) => item[1])
-}
-
-// function str2ab(str: string) {
-//   var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-//   var bufView = new Uint16Array(buf);
-//   for (var i = 0, strLen = str.length; i < strLen; i++) {
-//     bufView[i] = str.charCodeAt(i);
-//   }
-//   return buf;
-// }
 
 const getDirectories = (source:string) =>
   readdirSync(source, { withFileTypes: true })
@@ -70,26 +62,6 @@ server.listen(port, () => {
   console.log(error.getMessage())
 }
 
-const cleanClients = () => {
-  getCli().filter(client => (client.room === '' && client.ws.readyState !== 1) || (client.room.length > 0 && Date.now() - client.used > CLIENT_USED)).forEach(client => delete clients[client.index] )
-  // clients.filter(client => client.ws.readyState !== 1).forEach(find => {const index = clients.findIndex(client => client.index === find.index); clients.splice(index,1)})
-}
-
-const sendPeers = () => {
-  const ready = getCli().filter(client => client.room === '' && client.name !== '' && client.ws.readyState === 1)
-  ready.forEach(item => item.ws.send(JSON.stringify ({do: 'peers', peers: ready.map(client => {return {name: client.name, index: client.index}}), count: getCli().filter(itm => itm.room === '' && itm.ws.readyState === 1).length})))
-  return Array.isArray(ready)
-}
-
-const isInCli = (index: string | Array<string>) => {
-  const arr = Object.keys(clients)
-if(!Array.isArray(index)) return arr.includes(index)
-  for(const item of index){
-    if(!arr.includes(item)) return false
-  }
-  return true
-}
-
 let maxId = 0
 
 const wss = new WebSocketServer({
@@ -99,37 +71,41 @@ const wss = new WebSocketServer({
 })
 
 try {
-// wss.on("upgrade", (request, socket, head) => {
-//   wss.handleUpgrade(request, socket, head, (websocket) => {
-    // const cookies = cookie.parse(request.headers.cookie || '');
-    // const index = cookies.index ? cookies.index : ''
-    // if(index!=='')
-    //   clients[index].ws = websocket
-    // const room = clients[index].room;
-    // if(room)
-    //   getCli().find((client:client) => client.room === room && client.index !== index)?.ws.send(JSON.stringify({do:'help-me'}))
-    // else
-    //   clients[index] = {index: index, name:'', ws:websocket, room:'', alive: true, id: 0, usePing: false, used: Date.now()}
-//       wss.emit("connection", websocket, request)
-//   })
-// })
 
 wss.on('connection', (ws) => {
-  // clients.forEach((item, index)=>{if([2,3].includes(item.ws.readyState)) clients.splice(index,1)})
 
-  ws.on('message', function message(data) {
+  ws.on('message', async function message(data) {
     const parsed = JSON.parse(data.toString())
     const keys = Object.keys(parsed)
 
-  if(! (keys.includes('do'))) return
+    const getCli = () => {
+      return (Object.entries(clients) as Array<[string, client]>).map((item:[string, client]) => item[1])
+    }
+    
+    const cleanClients = () => {
+      getCli().filter(client => (client.room === '' && client.ws.readyState !== 1) || (client.room.length > 0 && Date.now() - client.used > CLIENT_USED)).forEach(client => delete clients[client.index] )
+    }
+    
+    const sendPeers = () => {
+      const ready = getCli().filter(client => client.room === '' && client.name !== '' && client.ws.readyState === 1)
+      ready.forEach(item => item.ws.send(JSON.stringify ({do: 'peers', peers: ready.map(client => {return {name: client.name, index: client.index}}), count: getCli().filter(itm => itm.room === '' && itm.ws.readyState === 1).length})))
+      return Array.isArray(ready)
+    }
+    
+    const isInCli = (index: string | Array<string>) => {
+      const arr = Object.keys(clients)
+      if(!Array.isArray(index)) return arr.includes(index)
+        for(const item of index){
+          if(!arr.includes(item)) return false
+        }
+      return true
+    }
+    
+    if(! (keys.includes('do'))) return
 
-  // if(intr === undefined) {
-  //   startPingPong()
-  // }
-  
   if(!keys.includes('index')) return
-  if(!isInCli(parsed.index))
-    if(['help-me', 'nick', 'reset', 'check'].includes(parsed.do)) {
+  if(!isInCli(parsed.index)) {
+    if(['help-me', 'nick', 'reset', 'check', 'ping'].includes(parsed.do)) {
       clients[parsed.index] = {index: parsed.index, name:'', ws:ws, room: keys.includes('room') ? parsed.room : '', alive: true, id: maxId++, usePing: false, used: Date.now()}
       if(parsed.do === 'reset' && parsed.room !== '') {
         const found =  getCli().find(clnt => parsed.room === clnt.room && clnt.index && clnt.index !== parsed.index && clnt.ws.readyState === 1)
@@ -141,16 +117,21 @@ wss.on('connection', (ws) => {
       sendPeers()
       return
     }
-  else
-    if(clients[parsed.index].ws.readyState > 1)
+  }
+  else {
+    if(!clients[parsed.index].ws.readyState || clients[parsed.index].ws.readyState > 1)
       clients[parsed.index].ws = ws
     else {
       if(clients[parsed.index].ws !== ws) {
-        ws.send(JSON.stringify(({do:'reset', room: clients[parsed.index].room})))
-        ws.close()
+        clients[parsed.index].ws.close()
+        clients[parsed.index].ws = ws
         cleanClients()
+        ws.send(JSON.stringify(({do:'reset', room: clients[parsed.index].room})))
       }
     }
+  }
+
+  console.info(parsed)
   switch(parsed.do) {
 
     case 'nick':
@@ -235,58 +216,64 @@ wss.on('connection', (ws) => {
       sendPeers()
     break
 
-      case 'start':
-        if(keys.includes('with') && keys.includes('from')) {
-          const indexes = getCli().filter(elem => [parsed['with'], parsed['from']].includes(elem.name))
-          if(indexes.length > 0) {
-            indexes.map(client => {
-            if(JSON.stringify(client.ws) !== JSON.stringify(ws)) {
-              client.room = randomBytes(8).toString('hex')
-              client.ws.send(JSON.stringify({start:'now'}))
-          }})
-        }}
-        break;
-
-        case 'move':
-          if(!keys.includes('move') || !keys.includes('id')) return
-          const room = clients[parsed.index].room
-          if(room === '') return
-          const partners = getCli().filter(client => room === client.room && client.index !== parsed.index)
-          if(!partners) return
-          clients[parsed.index].ws.send(JSON.stringify({do: 'confirm', id: parsed.id, room: room}))
-          partners.forEach(cli => {
-            cli.ws.send(JSON.stringify({do:'move', move: parsed.move, room: room}))
-          })
-        break
-
-        case 'replace-phalanx':
-          if(!keys.includes('figure')) return
-          getCli().filter(client => client.room === clients[parsed.index].room && client.index !== clients[parsed.index].index).forEach(cli => cli.ws.send(JSON.stringify({do:'replace-phalanx', figure: parsed.figure})))
-          break
-
-        case 'reconnect':
-          break
-
-        case 'pong':
-          clients[parsed.index].alive = true
-        break
-
-        case 'ping':
-          clients[parsed.index].ws.send(JSON.stringify({do: 'pong', count: getCli().filter(client => client.room === clients[parsed.index].room && client.ws.readyState === 1).length}))
-        break
-
-        case 'help-me':
-          const found = getCli().find(clnt => clients[parsed.index].room === clnt.room && parsed.index !== clnt.index && clnt.ws.readyState === 1)
-          found?.ws.send(JSON.stringify({do:'help-me', index: (keys.includes('to') && isInCli(parsed.to) && clients[parsed.to].room === clients[parsed.index].room && clients[parsed.to].ws.readyState === 1) ? parsed.to : found.index, to: parsed.index}))
-        break
-
-        case 'help-sent':
-          if(!keys.includes('to') || !keys.includes('board') || !keys.includes('lastMove') || !keys.includes('playing') || !keys.includes('time')) return
-          clients[parsed.to].ws.send(JSON.stringify({do:'help-sent', board: parsed.board, lastMove: parsed.lastMove, playing: parsed.playing, time: parsed.time}))
-        break
+    case 'start':
+      if(keys.includes('with') && keys.includes('from')) {
+        const indexes = getCli().filter(elem => [parsed['with'], parsed['from']].includes(elem.name))
+        if(indexes.length > 0) {
+          indexes.map(client => {
+          if(JSON.stringify(client.ws) !== JSON.stringify(ws)) {
+            client.room = randomBytes(8).toString('hex')
+            client.ws.send(JSON.stringify({start:'now'}))
+        }})
       }
     }
-  )  
+    break;
+
+    case 'move':
+      if(!keys.includes('move') || !keys.includes('id')) return
+      const room = clients[parsed.index].room
+      if(room === '') return
+      const partners = getCli().filter(client => room === client.room && client.index !== parsed.index)
+      if(!partners) return
+      clients[parsed.index].ws.send(JSON.stringify({do: 'confirm', id: parsed.id, room: room}))
+      partners.forEach(cli => {
+        cli.ws.send(JSON.stringify({do:'move', move: parsed.move, room: room}))
+      }
+    )
+    break
+
+    case 'replace-phalanx':
+      if(!keys.includes('figure')) return
+      getCli().filter(client => client.room === clients[parsed.index].room && client.index !== clients[parsed.index].index).forEach(cli => cli.ws.send(JSON.stringify({do:'replace-phalanx', figure: parsed.figure})))
+    break
+
+    case 'reconnect':
+    break
+
+    case 'pong':
+      clients[parsed.index].alive = true
+    break
+
+    case 'ping':
+      clients[parsed.index].ws.send(JSON.stringify({do: 'pong', count: getCli().filter(client => client.room === clients[parsed.index].room && client.ws.readyState === 1).length, room: clients[parsed.index].room}))
+    break
+
+    case 'help-me':
+      if(clients[parsed.index].room?.length > 8) {
+        const found = getCli().find(clnt => clients[parsed.index].room === clnt.room && parsed.index !== clnt.index && clnt.ws.readyState === 1)
+        found?.ws.send(JSON.stringify({do:'help-me', index: (keys.includes('to') && isInCli(parsed.to) && clients[parsed.to].room === clients[parsed.index].room && clients[parsed.to].ws.readyState === 1) ? parsed.to : found.index, to: parsed.index}))
+      }
+      else sendPeers()
+    break
+
+    case 'help-sent':
+      if(!keys.includes('to') || !keys.includes('board') || !keys.includes('lastMove') || !keys.includes('playing') || !keys.includes('time')) return
+      clients[parsed.to].ws.send(JSON.stringify({do:'help-sent', board: parsed.board, lastMove: parsed.lastMove, playing: parsed.playing, time: parsed.time}))
+    break
+  }
+  await redis.set('clients', JSON.stringify(clients))
+}
+)  
 
   // const startPingPong = () => {
   //   intr = setInterval( () => {
@@ -314,4 +301,10 @@ wss.on('connection', (ws) => {
   console.log(error.getMessage())
 }
 
+}
+
+const app = express()
+const redis = createClient()
+redis.on('error', (err) => console.log('Redis Client Error', err));
+appl()
 export default app
